7bd6327f5260115ac4e52e08493a777facc0cda4
Voxel07 <voxel@vivaldi.net>
2020-04-26 15:32:51 +0200

    Event´s

--- mySite/event.php
+++ mySite/event.php
@@ -65,21 +65,44 @@ $stmt = mysqli_stmt_init($conn);
         $maxAnz = 0;
         mysqli_stmt_execute($stmt); 
         $events = mysqli_stmt_get_result($stmt);
+
+        // Rrueckmeldung an den Nutzer ob die eingabe erfolgreich war
+        if(isset($_GET['gelöscht'])){
+            echo'<div class ="rueckmeldung">
+                    Event '.$_GET["gelöscht"].' wurde erfolgreich gelöscht
+                </div>';
+        }
+        elseif(isset($_GET['eigetragen'])){
+            echo'<div class ="rueckmeldung">
+                    Event '.$_GET["eigetragen"].' wurde erfolgreich erstellt
+                </div>';
+        }
+        elseif(isset($_GET['update'])){
+            echo'<div class ="rueckmeldung">
+                    Event '.$_GET["update"].' wurde erfolgreich aktualisiert
+                </div>';
+        }
+        
+        
+      
+
+        echo'<h1 class="insetshadow">Anstehende Events</h1>';
         while ($aktEvent = mysqli_fetch_assoc($events)){
 
-            if($maxAnz < 4){
+            if($maxAnz < 10){
                 $td = makeDifferenz(time(), strtotime($aktEvent['datum'])); 
                 echo'
-                <div id="'.$aktEvent['id'].'" class="event">
-                    <div class="event-name"><a href="'.$aktEvent['link'].'">'.$aktEvent['name'].'</a></div>
-                    <div class="event-date">'.date("d-m-Y",strtotime($aktEvent['datum'])).'</div>
+                <div class="event">
+                    <div class="event-name"><a href="'.$aktEvent['link'].'">'.str_replace("-"," ",$aktEvent['name']).'</a></div>
+                    <div class="event-date">'.date("d:m:Y",strtotime($aktEvent['datum'])).'</div>
                     <div class="event-count">'.$td['day'][0] . ' ' . $td['day'][1] . ', ' . $td['std'][0] . ' ' . $td['std'][1] . 
-                    ', ' . $td['min'][0] . ' ' . $td['min'][1] .'</div>
-                    <div class="event-löschen">X</div>
+                    '</div>
+                    <div class="event-editieren"><a href ="includes/event_verwaltung.inc.php?edit='.$aktEvent['id'].'&name='.$aktEvent['name'].'">Editieren</a></div>
+                    <div class="event-löschen"><a href ="includes/event_verwaltung.inc.php?löschen='.$aktEvent['id'].'&name='.$aktEvent['name'].'">Löschen</a></div>
                 </div>';
             }
             $maxAnz++;
-
+            
         }
     }
 
@@ -88,21 +111,31 @@ $stmt = mysqli_stmt_init($conn);
     </div>
     <div class="events-neu">
         <form action = "includes/event_verwaltung.inc.php?herkunft=Index.php" method="post">
-            <input type="text" name="ev-name" placeholder="Eventname" maxlength="30" required > 
-            <!-- <label id="label-Vorname" >Eventname</label>     -->
-            <input type="url" name="ev-link" placeholder="Link zur Eventseite">
-            <!-- <label id="label-Vorname" >Link zur Eventseite</label>    -->
-            <input type="date" name="ev-date" placeholder="Datum" maxlength="8" required >
-            <!-- <label id="label-Vorname" >Datum</label>    -->
-            <button type="submit" name="Neues_Event">Neues Event eintragen</button>    
+           
+            <?php 
+            if(isset($_GET["edit"])){
+              
+                echo'
+                <input type="text" name="ev-name" value="'.str_replace("-"," ",$_SESSION["name"]).'" placeholder="Eventname" maxlength="30" required >            
+                <input type="url" name="ev-link" value='.$_SESSION["link"].' placeholder="Link zur Eventseite">         
+                <input type="date" name="ev-date" value='.$_SESSION["datum"].' placeholder="Datum" maxlength="8" required >  
+                <input type="hidden" name="id" value='.$_SESSION['id'].' >';
+                echo'<button type="submit" name="update">Event Updaten</button>';    
+            }
+            else {
+                echo'
+                <input type="text" name="ev-name" placeholder="Eventname" maxlength="30" required >            
+                <input type="url" name="ev-link" placeholder="Link zur Eventseite">         
+                <input type="date" name="ev-date" placeholder="Datum" maxlength="8" required >  
+                <button type="submit" name="Neues_Event">Neues Event eintragen</button>';
+            }
+            ?>    
         </form>
         
     </div>
     
 
 </div>
-
-<!-- Git test -->
     
 <?php
 include_once "footer.php";
--- mySite/includes/event_verwaltung.inc.php
+++ mySite/includes/event_verwaltung.inc.php
@@ -6,9 +6,11 @@ session_start();
 
 
 <?php
+// Events einfügen
 if(isset($_POST['Neues_Event'])&&isset($_SESSION['User'])){
 
     $eventName = htmlspecialchars(stripcslashes(trim($_POST['ev-name'])));
+    $eventName = str_replace(" ","-",$eventName);
     $eventLink = htmlspecialchars(stripcslashes(trim($_POST['ev-link'])));
     $eventDate = htmlspecialchars(stripcslashes(trim($_POST['ev-date'])));
     $evErsteller = $_SESSION['User'];
@@ -45,28 +47,34 @@ if(isset($_POST['Neues_Event'])&&isset($_SESSION['User'])){
         }
 
     }
-    header("Location: ../event.php?Event eingetragen");
+    header("Location: ../event.php?eigetragen=$eventName");
     exit();
 }
-elseif ($_POST['Event_Löschen']&&isset($_SESSION['User'])) {
 
-    $evZumLöschen = htmlspecialchars(stripcslashes(trim($_POST['ev-name'])));
+// Events löschen
+// elseif ($GET['löschen']&&isset($_SESSION['User'])&&$_SESSION['rang']>=0) {
+elseif (isset($_GET['löschen'])) {
 
-    $sql = "DELETE FROM events WHERE event = ?";
+    $id = htmlspecialchars(stripcslashes(trim($_GET['löschen'])));
+    $evZumLöschen = htmlspecialchars(stripcslashes(trim($_GET['name'])));
+    $löscher = $_SESSION['User'];
+
+    $sql = "DELETE FROM events WHERE id = ?";
     $stmt = mysqli_stmt_init($conn);
     
     if(!mysqli_stmt_prepare($stmt,$sql)){
         echo'Kaputt';
-        header("Location: ../event.php?error=sql_error_insert");
+        header("Location: ../event.php?error=sql_error_löschen");
         exit();
     }
     else {
-        mysqli_stmt_bind_param($stmt,"s",$evZumLöschen);
+        mysqli_stmt_bind_param($stmt,"i",$id);
         mysqli_stmt_execute($stmt); 
 
         $filename = 'Events.txt';
         $date = date("d.m.Y - H:i", time());
-        $somecontent = "$date | Event: $evZumLöschen vom Nutzer: $evErsteller gelöscht\n";
+    
+        $somecontent = "$date | Event: $evZumLöschen vom Nutzer: $löscher gelöscht\n";
         
         if (is_writable("../../Logdaten/$filename")) {
             if (!$handle = fopen("../../Logdaten/$filename", "a")) {
@@ -83,10 +91,88 @@ elseif ($_POST['Event_Löschen']&&isset($_SESSION['User'])) {
             print "Die Datei $filename ist nicht schreibbar";
         }
 
+        header("Location: ../event.php?gelöscht=$evZumLöschen");
     }
 }
+
+//Event updaten
+// elseif ($GET['edit']&&isset($_SESSION['User'])&&$_SESSION['rang']>=0) {
+elseif(isset($_GET['edit'])){
+    $id = htmlspecialchars(stripcslashes(trim($_GET['edit'])));
+    $evZumÄndern = htmlspecialchars(stripcslashes(trim($_GET['name'])));
+
+    $sql ="SELECT * FROM events WHERE id =?";
+    $stmt = mysqli_stmt_init($conn);
+    
+    if(!mysqli_stmt_prepare($stmt,$sql)){
+        echo'Kaputt';
+        header("Location: ../event.php?error=sql_error_edit");
+        exit();
+    }
+    else {
+        mysqli_stmt_bind_param($stmt,"i",$id);
+        mysqli_stmt_execute($stmt); 
+      
+        $event = mysqli_stmt_get_result($stmt);
+        $row = mysqli_fetch_assoc($event);
+       
+        if($row != NULL){
+            // $row = mysqli_fetch_assoc($event);
+            $_SESSION['name'] = $row['name'];
+            $_SESSION['link'] = $row['link'];
+            $_SESSION['datum'] = $row['datum'];
+            $_SESSION['id']=$row['id'];
+
+          header("Location: ../event.php?edit=true");
+        }
+    }
+}
+//
+elseif (isset($_POST['update'])) {
+    $id = htmlspecialchars(stripcslashes(trim($_POST['id'])));
+    $editor = $_SESSION['User']; 
+
+    $eventName = htmlspecialchars(stripcslashes(trim($_POST['ev-name'])));
+    $eventName = str_replace(" ","-",$eventName);
+    $eventLink = htmlspecialchars(stripcslashes(trim($_POST['ev-link'])));
+    $eventDate = htmlspecialchars(stripcslashes(trim($_POST['ev-date'])));
+
+    $sql ="UPDATE events SET name = ?, link = ?, datum = ? WHERE id = ?";
+    $stmt = mysqli_stmt_init($conn);
+    
+    if(!mysqli_stmt_prepare($stmt,$sql)){
+        header("Location: ../event.php?error=sql_error_update".mysqli_error($conn));
+        exit();
+    }
+    else {
+        mysqli_stmt_bind_param($stmt,"sssi",$eventName,$eventLink,$eventDate, $id);
+        mysqli_stmt_execute($stmt); 
+
+        
+        $filename = 'Events.txt';
+        $date = date("d.m.Y - H:i", time());
+        $somecontent = "$date | Event: $eventName vom Nutzer: $editor bearbeitet\n";
+        
+        if (is_writable("../../Logdaten/$filename")) {
+            if (!$handle = fopen("../../Logdaten/$filename", "a")) {
+                    print "Kann die Datei $filename nicht öffnen";
+                    exit();
+            }
+            if (!fwrite($handle, $somecontent)) {
+                print "Kann in die Datei $filename nicht schreiben";
+                exit();
+            }
+            fclose($handle);
+        
+        } else {
+            print "Die Datei $filename ist nicht schreibbar";
+        }
+        header("Location: ../event.php?update=$eventName ");
+    }
+}
+
 else {
-    header("Location: ../event.php?läuft");
+    header("Location: ../event.php?was willst du hier");
 }
 
 ?>
